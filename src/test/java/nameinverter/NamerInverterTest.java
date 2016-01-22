633db22443712a75cacc5de6f7f82c85b456cfa1
mikael.baek <mikael.baek@daumkakao.com>
2016-01-22 15:24:14 +0900

    given_null___returns_empty_string  - 문제정의(add failing test). Red Phase

--- src/test/java/nameinverter/NamerInverterTest.java
+++ src/test/java/nameinverter/NamerInverterTest.java
@@ -1,4 +1,10 @@
 package nameinverter;
 
+import org.junit.Test;
+
 public class NamerInverterTest {
+	@Test
+	public void given_null___returns_empty_string() throws Exception {
+		assertThat(invert(null), is(""));
+	}
 }

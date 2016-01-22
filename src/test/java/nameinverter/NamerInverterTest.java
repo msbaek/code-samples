4f53711d641ca8a73e28d327f2774b3767d84b95
mikael.baek <mikael.baek@daumkakao.com>
2016-01-22 16:37:03 +0900

    given_honorific_and_first_last___returns_last_first - Green Phase
    - asList는 unmodifiable list를 반환하여 runtime 오류가 발생. 이를 해소.

--- src/test/java/nameinverter/NamerInverterTest.java
+++ src/test/java/nameinverter/NamerInverterTest.java
@@ -2,6 +2,7 @@ package nameinverter;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -48,7 +49,7 @@ public class NamerInverterTest {
 		if (name == null || name.isEmpty())
 			return "";
 		else {
-			List<String> names = Arrays.asList(name.trim().split("\\s+"));
+			List<String> names = new ArrayList<>(Arrays.asList(name.trim().split("\\s+")));
 			if (names.size() > 1 && names.get(0).equals("Mr."))
 				names.remove(0);
 			if (names.size() == 1)

5d590d9135f57b863d93309e3ffed302349a5415
mikael.baek <mikael.baek@daumkakao.com>
2016-02-11 18:26:36 +0900

    make it work

---
+++ src/main/java/MyStack.java
@@ -0,0 +1,8 @@
+public class MyStack {
+	public void push(int element) {
+	}
+
+	public int pop() {
+		return 99;
+	}
+}
--- src/test/java/StackTest.java
+++ src/test/java/StackTest.java
@@ -1,9 +1,17 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class StackTest {
+	private MyStack stack;
+
+	@Before
+	public void setUp() throws Exception {
+		stack = new MyStack();
+	}
+
 	@Test public void afterPushingX_willPopX() {
 		stack.push(99);
 		assertThat(stack.pop(), is(99));

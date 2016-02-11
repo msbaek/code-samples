f7e5468396d412bee2ba75177c8ccf519c2046a0
mikael.baek <mikael.baek@daumkakao.com>
2016-02-11 12:51:06 +0900

    mailbox를 local variable로 선언(quick fix를 이용)

--- src/test/java/MailTest.java
+++ src/test/java/MailTest.java
@@ -1,7 +1,11 @@
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class MailTest {
 	@Test public void newMailBox_isEmpty() {
+		MailBox mailbox;
 		assertThat(mailbox.messageCount(), is(0));
 	}
 }

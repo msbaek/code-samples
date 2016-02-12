e1ee28d1686db67e3ffcb60b597294f33ac6a27d
mikael.baek <mikael.baek@daumkakao.com>
2016-02-12 14:09:48 +0900

    7.1 make it pass by duplicating

--- src/test/java/PrimeFactorsTest.java
+++ src/test/java/PrimeFactorsTest.java
@@ -27,6 +27,8 @@ public class PrimeFactorsTest {
 		if(n > 1) {
 			for(; n % 2 == 0; n /= 2)
 				factors.add(2);
+			for(; n % 3 == 0; n /= 3)
+				factors.add(3);
 		}
 		if(n > 1)
 			factors.add(n);

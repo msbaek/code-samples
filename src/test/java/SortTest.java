920ece7c79057640dd8cfde683b4b28765589f88
mikael.baek <mikael.baek@daumkakao.com>
2016-02-15 11:31:24 +0900

    ### 4.2 refactoring - extract methods, while to for

--- src/test/java/SortTest.java
+++ src/test/java/SortTest.java
@@ -24,15 +24,20 @@ public class SortTest {
 	}
 
 	private List<Integer> sort(List<Integer> list) {
-		int index = 0;
-		while(index < list.size() - 1) {
-			if(list.get(index) > list.get(index + 1)) {
-				int temp = list.get(index);
-				list.set(index, list.get(index + 1));
-				list.set(index + 1, temp);
-			}
-			index++;
-		}
+		for(int index = 0; index < list.size() - 1; index++)
+			if(outOfOrder(list, index))
+				swap(list, index);
+
 		return list;
+	}
+
+	private boolean outOfOrder(List<Integer> list, int index) {
+		return list.get(index) > list.get(index + 1);
+	}
+
+	private void swap(List<Integer> list, int index) {
+		int temp = list.get(index);
+		list.set(index, list.get(index + 1));
+		list.set(index + 1, temp);
 	}
 }

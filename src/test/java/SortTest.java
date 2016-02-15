ea399649ddb3b4998b4f87f266980652ea7c1111
mikael.baek <mikael.baek@daumkakao.com>
2016-02-15 12:51:24 +0900

    ### 6 big list sort

--- src/test/java/SortTest.java
+++ src/test/java/SortTest.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -14,6 +15,16 @@ public class SortTest {
 		assertSorted(intList(2, 1), intList(1, 2));
 		assertSorted(intList(1, 3, 2), intList(1, 2, 3));
 		assertSorted(intList(3, 2, 1), intList(1, 2, 3));
+		sortBig(10000);
+	}
+
+	private void sortBig(int n) {
+		List<Integer> unsorted = new ArrayList<>();
+		for(int i = 0; i < n; i++)
+			unsorted.add((int)(Math.random() * 10000.0));
+		List<Integer> sorted = sort(unsorted);
+		for(int i = 0; i < n - 1; i++)
+			assertThat(sorted.get(i) <= sorted.get(i + 1), is(true));
 	}
 
 	private void assertSorted(List<Integer> unsorted, List<Integer> sorted) {

2189fdcf4101117c18f48c5c07e8a2386115a98a
mikael.baek <mikael.baek@daumkakao.com>
2016-02-15 11:00:00 +0900

    ### 3.2 transform - split flow, assign

--- src/test/java/SortTest.java
+++ src/test/java/SortTest.java
@@ -19,6 +19,13 @@ public class SortTest {
 	}
 
 	private List<Integer> sort(List<Integer> list) {
+		if(list.size() > 1) {
+			if(list.get(0) > list.get(1)) {
+				int temp = list.get(0);
+				list.set(0, list.get(1));
+				list.set(1, temp);
+			}
+		}
 		return list;
 	}
 }

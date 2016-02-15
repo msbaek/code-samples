e235ba7bef09fea081fd4723feb13721d17c8548
mikael.baek <mikael.baek@daumkakao.com>
2016-02-15 10:26:49 +0900

    ### 1.2 transform - null

--- src/test/java/SortTest.java
+++ src/test/java/SortTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -9,5 +10,9 @@ public class SortTest {
 	@Test
 	public void sortings() {
 		assertThat(sort(Arrays.asList()), is(Arrays.asList()));
+	}
+
+	private List<Integer> sort(List<Integer> list) {
+		return null;
 	}
 }

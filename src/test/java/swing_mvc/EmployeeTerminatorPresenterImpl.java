611ee6d68e8757170c75eddcd305df352c69747c
mikael.baek <mikael.baek@daumkakao.com>
2016-04-14 16:42:22 +0900

    make EmployeeTerminatorPresenterImplTest pass

--- src/test/java/swing_mvc/EmployeeTerminatorPresenterImpl.java
+++ src/test/java/swing_mvc/EmployeeTerminatorPresenterImpl.java
@@ -3,14 +3,29 @@ package swing_mvc;
 import java.util.Vector;
 
 public class EmployeeTerminatorPresenterImpl implements EmployeeTerminatorPresenter {
-	public void initialize(Vector noEmployees, EmployeeTerminatorView view) {
+	private EmployeeTerminatorView view;
+	private Vector employees;
+	private String selectedEmployee;
+
+	public void initialize(Vector employees, EmployeeTerminatorView view) {
+		this.employees = employees;
+		this.view = view;
+		view.setEmployeeList(employees);
+		view.clearSelection();
+		view.enableTerminate(false);
 	}
 
-	@Override
+	// EmployeeTerminatorController interface
 	public void selectionChanged(String employee) {
+		view.enableTerminate(employee != null);
+		selectedEmployee = employee;
 	}
 
-	@Override
 	public void terminate() {
+		if (selectedEmployee != null)
+			employees.remove(selectedEmployee);
+		view.setEmployeeList(employees);
+		view.clearSelection();
+		view.enableTerminate(false);
 	}
 }

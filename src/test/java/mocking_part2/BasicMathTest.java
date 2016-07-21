92fc215c19209d08464682798ac58898797fbded
mikael.baek <mikael.baek@daumkakao.com>
2016-07-21 16:17:25 +0900

    상태 확인을 상호작용 확인으로 변경 - introduce interface - add spy

--- src/test/java/mocking_part2/BasicMathTest.java
+++ src/test/java/mocking_part2/BasicMathTest.java
@@ -3,27 +3,54 @@ package mocking_part2;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 public class BasicMathTest {
 	private BasicMath basicMath;
+	private TestingAdder adder;
 
 	@Before
 	public void setUp() throws Exception {
+		adder = new TestingAdder();
 		basicMath = new BasicMath();
+		basicMath.setAdder(adder);
 	}
 
 	@Test
-	public void small_number() {
+	public void test_interaction_not_state() {
 		int a = 3;
 		int b = 4;
-		int result = basicMath.multiply(a, b);
+		basicMath.multiply(a, b);
 
-		assertThat(result, is(12));
+		assertThat(adder.getTraceList().toString(), is("[ 0 +  4,  4 +  4,  8 +  4]"));
+	}
+
+	class TestingAdder implements Adder {
+		public List<String> traceList = new ArrayList<>();
+
+		@Override
+		public int add(int p, int b) {
+			traceList.add(String.format("%2d + %2d", p, b));
+			return p + b;
+		}
+
+		public List<String> getTraceList() {
+			return traceList;
+		}
 	}
 
 	class BasicMath {
+
+		private Adder adder;
+
+		public BasicMath() {
+			this.adder = (p, b) -> p + b;
+		}
+
 		private int multiply(int a, int b) {
 			boolean posA = a >= 0;
 			boolean posB = b >= 0;
@@ -34,13 +61,18 @@ public class BasicMathTest {
 
 			int p = 0;
 			while (a-- > 0)
-				p = add(p, b);
+				p = adder.add(p, b);
 
 			return negP ? -p : p;
 		}
-
-		private int add(int p, int b) {
-			return p + b;
+		public void setAdder(Adder adder) {
+			this.adder = adder;
 		}
+
+	}
+
+	interface Adder {
+		int add(int p, int b);
+
 	}
 }

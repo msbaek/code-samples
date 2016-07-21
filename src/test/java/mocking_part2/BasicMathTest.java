618584ac11d087786f6364cfc82c68c1fdfdd844
mikael.baek <mikael.baek@daumkakao.com>
2016-07-21 16:50:57 +0900

    self shunt

--- src/test/java/mocking_part2/BasicMathTest.java
+++ src/test/java/mocking_part2/BasicMathTest.java
@@ -9,15 +9,13 @@ import java.util.List;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
-public class BasicMathTest {
+public class BasicMathTest implements Adder {
 	private BasicMath basicMath;
-	private TestingAdder adder;
 
 	@Before
 	public void setUp() throws Exception {
-		adder = new TestingAdder();
 		basicMath = new BasicMath();
-		basicMath.setAdder(adder);
+		basicMath.setAdder(this);
 	}
 
 	@Test
@@ -26,53 +24,48 @@ public class BasicMathTest {
 		int b = 4;
 		basicMath.multiply(a, b);
 
-		assertThat(adder.getTraceList().toString(), is("[ 0 +  4,  4 +  4,  8 +  4]"));
+		assertThat(traceList.toString(), is("[ 0 +  4,  4 +  4,  8 +  4]"));
 	}
 
-	class TestingAdder implements Adder {
-		public List<String> traceList = new ArrayList<>();
-
-		@Override
-		public int add(int p, int b) {
-			traceList.add(String.format("%2d + %2d", p, b));
-			return p + b;
-		}
-
-		public List<String> getTraceList() {
-			return traceList;
-		}
+	@Override
+	public int add(int p, int b) {
+		traceList.add(String.format("%2d + %2d", p, b));
+		return p + b;
 	}
 
-	class BasicMath {
+	List<String> traceList = new ArrayList<>();
+}
 
-		private Adder adder;
+class BasicMath {
 
-		public BasicMath() {
-			this.adder = (p, b) -> p + b;
-		}
+	private Adder adder;
 
-		private int multiply(int a, int b) {
-			boolean posA = a >= 0;
-			boolean posB = b >= 0;
-			boolean negP = posA != posB;
-
-			a = posA ? a : -a;
-			b = posB ? b : -b;
-
-			int p = 0;
-			while (a-- > 0)
-				p = adder.add(p, b);
-
-			return negP ? -p : p;
-		}
-		public void setAdder(Adder adder) {
-			this.adder = adder;
-		}
-
+	public BasicMath() {
+		this.adder = (p, b) -> p + b;
 	}
 
-	interface Adder {
-		int add(int p, int b);
+	public int multiply(int a, int b) {
+		boolean posA = a >= 0;
+		boolean posB = b >= 0;
+		boolean negP = posA != posB;
 
+		a = posA ? a : -a;
+		b = posB ? b : -b;
+
+		int p = 0;
+		while (a-- > 0)
+			p = adder.add(p, b);
+
+		return negP ? -p : p;
 	}
+
+	public void setAdder(Adder adder) {
+		this.adder = adder;
+	}
+
+}
+
+interface Adder {
+	int add(int p, int b);
+
 }

0d2ad1d0c747e0549d4d01a734b7caa72fc14c16
mikael.baek <mikael.baek@daumkakao.com>
2016-09-29 18:23:43 +0900

    add custom_annotation.Test annotation interface

---
+++ src/test/java/custom_annotation/Test.java
@@ -0,0 +1,20 @@
+package custom_annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+// https://www.mkyong.com/java/java-custom-annotations-example/
+
+@Retention(RetentionPolicy.RUNTIME)
+// can use in method only
+@Target(ElementType.METHOD)
+public @interface Test {
+	// should ignore this test ?
+	public boolean enabled() default true;
+	/**
+	 * Method declarations must not have any parameters or a throws clause. Return types are restricted to primitives, String, Class, enums,
+	 * annotations, and arrays of the preceding types.
+	 */
+}

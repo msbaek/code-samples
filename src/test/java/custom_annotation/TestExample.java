f4bfe1c4f47c782d4f545ef4eb54447f244aedf4
mikael.baek <mikael.baek@daumkakao.com>
2016-09-29 18:41:10 +0900

    add custom_annotation.TestExample class

---
+++ src/test/java/custom_annotation/TestExample.java
@@ -0,0 +1,26 @@
+package custom_annotation;
+
+@TesterInfo( //
+priority = TesterInfo.Priority.HIGH, //
+createdBy = "mkyong.com", //
+tags = { "sales", "test" })
+public class TestExample {
+	@Test
+	void testA() {
+		if (true)
+			throw new RuntimeException("This test always failed");
+	}
+
+	@Test(enabled = false)
+	void testB() {
+		if (false)
+			throw new RuntimeException("This test always passed");
+	}
+
+	@Test(enabled = true)
+	void testC() {
+		if (10 > 1) {
+			// do nothing, this test always passed.
+		}
+	}
+}

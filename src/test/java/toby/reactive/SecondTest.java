f5d4249abe8f9ee7d8731ce3d9c06f5f6e6adb3f
mikael.baek <mikael.baek@daumkakao.com>
2017-01-16 20:54:25 +0900

    add SecondTest

---
+++ src/test/java/toby/reactive/SecondTest.java
@@ -0,0 +1,63 @@
+package toby.reactive;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Slf4j
+public class SecondTest {
+	@Test
+	public void pub_sub_test() {
+		Publisher<Integer> pub = new Publisher<Integer>() {
+			Iterable<Integer> iter = Stream.iterate(1, s -> s + 1).limit(10).collect(Collectors.toList());
+
+			@Override
+			public void subscribe(Subscriber<? super Integer> sub) {
+				sub.onSubscribe(new Subscription() {
+					@Override
+					public void request(long n) {
+						try {
+							iter.forEach(s -> sub.onNext(s));
+							sub.onComplete();
+						} catch (Exception e) {
+							sub.onError(e);
+						}
+					}
+
+					@Override
+					public void cancel() {
+					}
+				});
+			}
+		};
+
+		Subscriber<Integer> sub = new Subscriber<Integer>() {
+			@Override
+			public void onSubscribe(Subscription s) {
+				s.request(Long.MAX_VALUE);
+			}
+
+			@Override
+			public void onNext(Integer i) {
+				log.debug("onNext: {}", i);
+			}
+
+			@Override
+			public void onError(Throwable t) {
+				log.debug("onError: {}", t);
+			}
+
+			@Override
+			public void onComplete() {
+				log.debug("onComplete");
+			}
+		};
+
+		pub.subscribe(sub);
+	}
+}

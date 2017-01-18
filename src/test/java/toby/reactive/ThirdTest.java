5b4a01dbe793da874f56bbd9667d5180f35947bc
mikael.baek <mikael.baek@daumkakao.com>
2017-01-18 19:15:08 +0900

    add subscriber

--- src/test/java/toby/reactive/ThirdTest.java
+++ src/test/java/toby/reactive/ThirdTest.java
@@ -1,10 +1,12 @@
 package toby.reactive;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 
+@Slf4j
 public class ThirdTest {
 	@Test
 	public void scheduler_example() {
@@ -28,5 +30,28 @@ public class ThirdTest {
 				});
 			}
 		};
+
+		pub.subscribe(new Subscriber<Integer>() {
+			@Override
+			public void onSubscribe(Subscription s) {
+				log.debug("onSubscribe");
+				s.request(Long.MAX_VALUE);
+			}
+
+			@Override
+			public void onNext(Integer integer) {
+				log.debug("onNext: {}", integer);
+			}
+
+			@Override
+			public void onError(Throwable t) {
+				log.debug("onError: {}", t);
+			}
+
+			@Override
+			public void onComplete() {
+				log.debug("onComplete");
+			}
+		});
 	}
 }

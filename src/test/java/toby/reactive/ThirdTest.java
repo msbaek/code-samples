6d76ca67fc1833d3037220d90db311a472e932a4
mikael.baek <mikael.baek@daumkakao.com>
2017-01-18 19:17:42 +0900

    apply lambda

--- src/test/java/toby/reactive/ThirdTest.java
+++ src/test/java/toby/reactive/ThirdTest.java
@@ -10,25 +10,22 @@ import org.reactivestreams.Subscription;
 public class ThirdTest {
 	@Test
 	public void scheduler_example() {
-		Publisher<Integer> pub = new Publisher<Integer>() {
-			@Override
-			public void subscribe(Subscriber<? super Integer> sub) {
-				sub.onSubscribe(new Subscription() {
-					@Override
-					public void request(long n) {
-						sub.onNext(1);
-						sub.onNext(2);
-						sub.onNext(3);
-						sub.onNext(4);
-						sub.onNext(5);
-						sub.onComplete();
-					}
+		Publisher<Integer> pub = sub -> {
+			sub.onSubscribe(new Subscription() {
+				@Override
+				public void request(long n) {
+					sub.onNext(1);
+					sub.onNext(2);
+					sub.onNext(3);
+					sub.onNext(4);
+					sub.onNext(5);
+					sub.onComplete();
+				}
 
-					@Override
-					public void cancel() {
-					}
-				});
-			}
+				@Override
+				public void cancel() {
+				}
+			});
 		};
 
 		pub.subscribe(new Subscriber<Integer>() {

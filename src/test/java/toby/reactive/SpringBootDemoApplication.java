77bf91806cb07f1c41a21c4eefde71fe05f5bd73
mikael.baek <mikael.baek@daumkakao.com>
2017-01-21 16:15:04 +0900

    make it asyn
    async 결과 처리 방안
    1. 결과를 DB에 넣고 DB 값을 조사
    2. Future를 HttpSession에 넣고, 이후 controller에서 조사

--- src/test/java/toby/reactive/SpringBootDemoApplication.java
+++ src/test/java/toby/reactive/SpringBootDemoApplication.java
@@ -7,19 +7,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.Future;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 
 @SpringBootApplication
 @Slf4j
+@EnableAsync
 public class SpringBootDemoApplication {
 	@Component
 	public static class MyService {
-		public String hello() throws InterruptedException {
+		@Async
+		public Future<String> hello() throws InterruptedException {
 			log.info("hello");
-			SECONDS.sleep(1);
-			return "Hello";
+			SECONDS.sleep(2);
+			return new AsyncResult<>("Hello");
 		}
 	}
 
@@ -34,8 +41,9 @@ public class SpringBootDemoApplication {
 	@Bean ApplicationRunner run() {
 		return args -> {
 			log.info("run()");
-			String result = myService.hello();
-			log.info("exit with result={}", result);
+			Future<String> result = myService.hello();
+			log.info("exit with isDone={}", result.isDone());
+			log.info("result={}", result.get());
 		};
 	}
 }

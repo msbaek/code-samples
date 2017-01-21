78bbbc2149078b09f46c3f775a66953b7989d53e
mikael.baek <mikael.baek@daumkakao.com>
2017-01-21 16:03:27 +0900

    add ApplicationRunner

--- src/test/java/toby/reactive/SpringBootDemoApplication.java
+++ src/test/java/toby/reactive/SpringBootDemoApplication.java
@@ -1,11 +1,21 @@
 package toby.reactive;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootDemoApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDemoApplication.class, args);
+		try (ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringBootDemoApplication.class, args)) {
+		}
+	}
+
+	// executed when spring boot start
+	@Bean ApplicationRunner run() {
+		return args -> {
+		};
 	}
 }

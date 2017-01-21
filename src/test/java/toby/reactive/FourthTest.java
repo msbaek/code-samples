8c7c57efa302e566e1eb6902aa152f9bf40a3eb8
mikael.baek <mikael.baek@daumkakao.com>
2017-01-21 10:32:25 +0900

    introduce CallbackFuture and SuccessCallback

--- src/test/java/toby/reactive/FourthTest.java
+++ src/test/java/toby/reactive/FourthTest.java
@@ -10,27 +10,39 @@ import static java.util.concurrent.TimeUnit.SECONDS;
 
 @Slf4j
 public class FourthTest {
+	interface SuccessCallback {
+		void onSuccess(String res);
+	}
+
+	public static class CallbackFuture extends FutureTask<String> {
+		private final SuccessCallback successCallback;
+
+		public CallbackFuture(Callable<String> callable, SuccessCallback successCallback) {
+			super(callable);
+			this.successCallback = successCallback;
+		}
+
+		@Override
+		protected void done() {
+			try {
+				successCallback.onSuccess(get());
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} catch (ExecutionException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
 	@Test
 	public void future_example() throws InterruptedException, ExecutionException {
 		ExecutorService es = Executors.newCachedThreadPool();
 
-		FutureTask<String> f = new FutureTask<String>(() -> {
+		CallbackFuture f = new CallbackFuture(() -> {
 			SECONDS.sleep(2);
-			log.debug("Hello");
+			log.debug("async");
 			return "Hello";
-		}) {
-			@Override
-			protected void done() {
-				try {
-					log.debug("result={}", get());
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				} catch (ExecutionException e) {
-					e.printStackTrace();
-				}
-			}
-		};
+		}, res -> log.debug("result={}", res));
 
 		es.execute(f);
 

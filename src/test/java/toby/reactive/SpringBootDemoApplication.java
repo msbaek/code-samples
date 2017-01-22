6f5f35bbf15131899b4a82521e4ca552f5e0ca06
mikael.baek <mikael.baek@daumkakao.com>
2017-01-22 10:12:06 +0900

    apply ThreadPoolTaskExecutor

--- src/test/java/toby/reactive/SpringBootDemoApplication.java
+++ src/test/java/toby/reactive/SpringBootDemoApplication.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 import org.springframework.util.concurrent.ListenableFuture;
 
 import java.util.concurrent.Future;
+import java.util.concurrent.ThreadPoolExecutor;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 
@@ -29,6 +31,19 @@ public class SpringBootDemoApplication {
 			SECONDS.sleep(2);
 			return new AsyncResult<>("Hello");
 		}
+	}
+
+	@Bean ThreadPoolTaskExecutor taskExecutor() {
+		// default로 제공되는 SimpleAsyncTaskExecutor는 요청이 올 때마다 thread를 생성함
+		// cpu, memory 측면에서 비효율적
+		ThreadPoolTaskExecutor te = new ThreadPoolTaskExecutor();
+		// core 만큼 초기 에 만들고 queue size만큼 큐잉하다가 maxsize만큼 생성
+		te.setCorePoolSize(10);
+		te.setMaxPoolSize(100);
+		te.setQueueCapacity(200);
+		te.setThreadNamePrefix("mythread");
+		te.initialize();
+		return te;
 	}
 
 	public static void main(String[] args) {

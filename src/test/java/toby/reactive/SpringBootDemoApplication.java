e3fcfaceb696f98cdee367eaa82add14a57dbd7d
mikael.baek <mikael.baek@daumkakao.com>
2017-01-23 22:07:44 +0900

    apply emitter

--- src/test/java/toby/reactive/SpringBootDemoApplication.java
+++ src/test/java/toby/reactive/SpringBootDemoApplication.java
@@ -4,12 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.async.DeferredResult;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
 
-import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.Executors;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 @SpringBootApplication
 @Slf4j
@@ -17,29 +18,22 @@ import java.util.concurrent.ConcurrentLinkedDeque;
 public class SpringBootDemoApplication {
 	@RestController
 	public static class MyController {
-		Queue<DeferredResult<String>> results = new ConcurrentLinkedDeque<>();
+		@RequestMapping("/emitter")
+		public ResponseBodyEmitter emitter() {
+			ResponseBodyEmitter emitter = new ResponseBodyEmitter();
 
-		@GetMapping("/dr")
-		public DeferredResult<String> dr() throws InterruptedException {
-			log.info("dr");
-			DeferredResult<String> dr = new DeferredResult<>(600_000L);
-			results.add(dr);
-			return dr;
-		}
+			Executors.newSingleThreadExecutor().submit(() -> {
+				try {
+					for (int i = 1; i <= 50; i++) {
+						emitter.send("<p>Stream " + i + "</p>");
+						MILLISECONDS.sleep(100);
+					}
+					emitter.complete();
+				} catch (Exception e) {
+				}
+			});
 
-		@GetMapping("/dr/count")
-		public String drcount() {
-			return String.valueOf(results.size());
-		}
-
-		@GetMapping("/dr/event")
-		public String drevent(String msg) {
-			for(DeferredResult<String> dr : results) {
-				dr.setResult("Hello " + msg);
-				results.remove(dr);
-			}
-
-			return "OK";
+			return emitter;
 		}
 	}
 

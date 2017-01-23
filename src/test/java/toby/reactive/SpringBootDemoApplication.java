52d59f01826419da941c6c13a50be6f55ba0ddf8
mikael.baek <mikael.baek@daumkakao.com>
2017-01-23 09:17:15 +0900

    prepare spring web demo

--- src/test/java/toby/reactive/SpringBootDemoApplication.java
+++ src/test/java/toby/reactive/SpringBootDemoApplication.java
@@ -1,21 +1,11 @@
 package toby.reactive;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.stereotype.Component;
-import org.springframework.util.concurrent.ListenableFuture;
-
-import java.util.concurrent.Future;
-import java.util.concurrent.ThreadPoolExecutor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 
@@ -23,47 +13,16 @@ import static java.util.concurrent.TimeUnit.SECONDS;
 @Slf4j
 @EnableAsync
 public class SpringBootDemoApplication {
-	@Component
-	public static class MyService {
-//		@Async
-		@Async(value = "taskExecutor")
-		public ListenableFuture<String> hello() throws InterruptedException {
-			log.info("hello");
+	@RestController
+	public static class MyController {
+		@GetMapping("/async")
+		public String async() throws InterruptedException {
 			SECONDS.sleep(2);
-			return new AsyncResult<>("Hello");
+			return "Hello from Async";
 		}
-	}
-
-	@Bean ThreadPoolTaskExecutor taskExecutor() {
-		// default로 제공되는 SimpleAsyncTaskExecutor는 요청이 올 때마다 thread를 생성함
-		// cpu, memory 측면에서 비효율적
-		ThreadPoolTaskExecutor te = new ThreadPoolTaskExecutor();
-		// core 만큼 초기 에 만들고 queue size만큼 큐잉하다가 maxsize만큼 생성
-		te.setCorePoolSize(10);
-		te.setMaxPoolSize(100);
-		te.setQueueCapacity(200);
-		te.setThreadNamePrefix("mythread");
-		te.initialize();
-		return te;
 	}
 
 	public static void main(String[] args) {
-		try (ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringBootDemoApplication.class, args)) {
-		}
-	}
-
-	@Autowired MyService myService;
-
-	// executed when spring boot start
-	@Bean ApplicationRunner run() {
-		return args -> {
-			log.info("run()");
-			ListenableFuture<String> f = myService.hello();
-			f.addCallback(
-				s -> log.info("success with result={}", s),
-				e -> log.error("error={}", e.getMessage())
-			);
-			log.info("exit before callback");
-		};
+		SpringApplication.run(SpringBootDemoApplication.class, args);
 	}
 }

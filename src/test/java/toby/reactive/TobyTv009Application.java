83651c4b0ac83a19afa572a0ad6d9dff474b7c57
mikael.baek <mikael.baek@daumkakao.com>
2017-02-02 09:51:02 +0900

    apply AsyncRestTemplate and ListenableFuture

--- src/test/java/toby/reactive/TobyTv009Application.java
+++ src/test/java/toby/reactive/TobyTv009Application.java
@@ -2,9 +2,11 @@ package toby.reactive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.AsyncRestTemplate;
 
 /**
  * 토비의 봄 TV 9회 - 스프링 리액티브 웹 개발 5부. 비동기 RestTemplate과 비동기 MVC의 결합
@@ -13,11 +15,11 @@ import org.springframework.web.client.RestTemplate;
 public class TobyTv009Application {
 	@RestController
 	public static class MyController {
-		RestTemplate rt = new RestTemplate();
+		AsyncRestTemplate rt = new AsyncRestTemplate();
 
 		@RequestMapping("/rest")
-		public String rest(int idx) {
-			return rt.getForObject("http://localhost:8081/service?req={req}", String.class, "hello" + idx);
+		public ListenableFuture<ResponseEntity<String>> rest(int idx) {
+			return rt.getForEntity("http://localhost:8081/service?req={req}", String.class, "hello" + idx);
 		}
 	}
 

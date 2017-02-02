7b599adb8f1e4a99d04ab3d5b8a628f5aa895489
mikael.baek <mikael.baek@daumkakao.com>
2017-02-02 21:32:45 +0900

    apply Netty4ClientHttpRequestFactory

--- src/test/java/toby/reactive/TobyTv009Application.java
+++ src/test/java/toby/reactive/TobyTv009Application.java
@@ -1,8 +1,10 @@
 package toby.reactive;
 
+import io.netty.channel.nio.NioEventLoopGroup;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.Netty4ClientHttpRequestFactory;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +17,7 @@ import org.springframework.web.client.AsyncRestTemplate;
 public class TobyTv009Application {
 	@RestController
 	public static class MyController {
-		AsyncRestTemplate rt = new AsyncRestTemplate();
+		AsyncRestTemplate rt = new AsyncRestTemplate(new Netty4ClientHttpRequestFactory(new NioEventLoopGroup(1)));
 
 		@RequestMapping("/rest")
 		public ListenableFuture<ResponseEntity<String>> rest(int idx) {

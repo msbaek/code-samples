09c12595427c9b2b1ef79c23be83c0fa1fffe2d8
mikael.baek <mikael.baek@daumkakao.com>
2017-04-01 15:34:20 +0900

    refactor StreamsGroupingByTest using consumer

--- src/test/java/java8/StreamsGroupingByTest.java
+++ src/test/java/java8/StreamsGroupingByTest.java
@@ -6,9 +6,12 @@ import org.junit.Test;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
+import java.util.function.Consumer;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * https://dzone.com/articles/java-streams-groupingby-examples
@@ -27,10 +30,9 @@ public class StreamsGroupingByTest {
     private String filename = "./players.csv";
     private final Pattern pattern = Pattern.compile(",");
 
-    @Test
-    public void group_by_year() throws Exception {
+    private void consumePlayerStreams(Consumer<Stream<Player>> c) throws IOException {
         try (BufferedReader in = new BufferedReader(new FileReader(filename))) {
-            Map<Integer, List<Player>> players = in
+            Stream<Player> playerStream = in
                     .lines()
                     .skip(1)
                     .map(line -> {
@@ -40,64 +42,41 @@ public class StreamsGroupingByTest {
                                 arr[2],
                                 arr[3],
                                 Integer.parseInt(arr[4]));
-                    })
-                    .collect(Collectors.groupingBy(p -> p.getYear()));
-            players
-                    .entrySet()
-                    .stream()
-                    .forEach(System.out::println);
+                    });
+
+            c.accept(playerStream);
         }
+    }
+
+    @Test
+    public void group_by_year() throws Exception {
+        consumePlayerStreams(playerStream -> playerStream
+                .collect(Collectors.groupingBy(Player::getYear))
+                .entrySet()
+                .forEach(System.out::println));
     }
 
     @Test
     public void group_by_year_and_teamId() throws Exception {
-        try (BufferedReader in = new BufferedReader(new FileReader(filename))) {
-            Map<List<String>, List<Player>> grouped = in
-                    .lines()
-                    .skip(1)
-                    .map(line -> {
-                        String[] arr = pattern.split(line);
-                        return new Player(Integer.parseInt(arr[0]),
-                                arr[1],
-                                arr[2],
-                                arr[3],
-                                Integer.parseInt(arr[4]));
-                    })
-                    .collect(Collectors.groupingBy(x -> new ArrayList<>(Arrays.asList(Integer.toString(x.getYear()), x.getTeamID()))));
-            grouped
-                    .entrySet()
-                    .stream()
-                    .forEach(x -> {
-                        System.out.println(x.getKey());
-                        x.getValue().stream()
-                                .forEach(p -> System.out.printf(" ( %2s %-10s %-10d )%n", p.getLgID(), p.getPlayerID(), p.getSalary()));
-                    });
-        }
+        consumePlayerStreams(playerStream -> playerStream
+                .collect(Collectors.groupingBy(x -> new ArrayList<>(Arrays.asList(Integer.toString(x.getYear()), x.getTeamID()))))
+                .entrySet()
+                .forEach(x -> {
+                    System.out.println(x.getKey());
+                    x.getValue()
+                            .forEach(p -> System.out.printf(" ( %2s %-10s %-10d )%n", p.getLgID(), p.getPlayerID(), p.getSalary()));
+                }));
     }
 
     @Test
     public void collect_into_a_set() throws Exception {
-        try (BufferedReader in = new BufferedReader(new FileReader(filename))) {
-            Map<List<String>, Set<Player>> grouped = in
-                    .lines()
-                    .skip(1)
-                    .map(line -> {
-                        String[] arr = pattern.split(line);
-                        return new Player(Integer.parseInt(arr[0]),
-                                arr[1],
-                                arr[2],
-                                arr[3],
-                                Integer.parseInt(arr[4]));
-                    })
-                    .collect(Collectors.groupingBy(x -> new ArrayList<>(Arrays.asList(x.getTeamID(), x.getLgID())),Collectors.toSet()));
-            grouped
-                    .entrySet()
-                    .stream()
-                    .forEach(x -> {
-                        System.out.println(x.getKey());
-                        x.getValue().stream()
-                                .forEach(p -> System.out.printf(" ( %2s %-10s %-10d )%n", p.getLgID(), p.getPlayerID(), p.getSalary()));
-                    });
-        }
+        consumePlayerStreams(playerStream -> playerStream
+                .collect(Collectors.groupingBy(x -> new ArrayList<>(Arrays.asList(x.getTeamID(), x.getLgID())), Collectors.toSet()))
+                .entrySet()
+                .forEach(x -> {
+                    System.out.println(x.getKey());
+                    x.getValue()
+                            .forEach(p -> System.out.printf(" ( %2s %-10s %-10d )%n", p.getLgID(), p.getPlayerID(), p.getSalary()));
+                }));
     }
 }

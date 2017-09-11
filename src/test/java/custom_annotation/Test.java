9f978d42c4297715cf569576f28384de6728af9a
백명석(Myeongseok Baek)/Tech Infra 개발본부/SKP <ms.baek@sk.com>
2017-09-11 19:10:55 +0900

    change comment format in custom_annotation

--- src/test/java/custom_annotation/Test.java
+++ src/test/java/custom_annotation/Test.java
@@ -8,13 +8,12 @@ import java.lang.annotation.Target;
 // https://www.mkyong.com/java/java-custom-annotations-example/
 
 @Retention(RetentionPolicy.RUNTIME)
-// can use in method only
-@Target(ElementType.METHOD)
+@Target(ElementType.METHOD) // can use in method only
 public @interface Test {
-	// should ignore this test ?
-	public boolean enabled() default true;
-	/**
-	 * Method declarations must not have any parameters or a throws clause. Return types are restricted to primitives, String, Class, enums,
-	 * annotations, and arrays of the preceding types.
-	 */
+    // should ignore this test ?
+    public boolean enabled() default true;
+    /**
+     * Method declarations must not have any parameters or a throws clause. Return types are restricted to primitives, String, Class, enums,
+     * annotations, and arrays of the preceding types.
+     */
 }
--- src/test/java/custom_annotation/TesterInfo.java
+++ src/test/java/custom_annotation/TesterInfo.java
@@ -6,8 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.TYPE)
-// on class level
+@Target(ElementType.TYPE) // on class level
 public @interface TesterInfo {
 	public enum Priority {
 		LOW, MEDIUM, HIGH
